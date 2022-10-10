21c14c5a2f73b41568e54c647c388f4b568f5cdb
pjaxon48 <59519727+pjaxon48@users.noreply.github.com>
2022-10-09 21:42:30 -0400

    Update DVA.py
    replaced round() with format() to fix display errors

--- DVA.py
+++ DVA.py
@@ -60,16 +60,16 @@ def twovectors():
     mB=float(math.sqrt(abs(Bi*Bi)+abs(Bj*Bj)+abs(Bk*Bk)))
     AdB=float(Ai*Bi+Aj*Bj+Ak*Bk)
     mAmB=float(mA*mB)
-    unvA=[round(Ai/mA,3),round(Aj/mA,3),round(Ak/mA,3)]
-    unvB=[round(Bi/mB,3),round(Bj/mB,3),round(Bk/mB,3)]
-    AcB=[round(Aj*Bk-Ak*Bj,3),round(Ak*Bi-Ai*Bk,3),round(Ai*Bj-Aj*Bi,3)]
-    print("magn.A= ",round(mA,3))
-    print("magn.B= ",round(mB,3))
+    unvA=[format(Ai/mA,'.3f'),format(Aj/mA,'.3f'),format(Ak/mA,'.3f')]
+    unvB=[format(Bi/mB,'.3f'),format(Bj/mB,'.3f'),format(Bk/mB,'.3f')]
+    AcB=[format(Aj*Bk-Ak*Bj,'.3f'),format(Ak*Bi-Ai*Bk,'.3f'),format(Ai*Bj-Aj*Bi,'.3f')]
+    print("magn.A= ",format(mA,'.3f'))
+    print("magn.B= ",format(mB,'.3f'))
     print("unit v.A= ",unvA[0],"i ",unvA[1],"j ",unvA[2],"k ")
     print("unit v.B= ",unvB[0],"i ",unvB[1],"j ",unvB[2],"k ")
     #print("unit v.A= ",round(Ai/mA,3),"i ",round(Aj/mA,3),"j ",round(Ak/mA,3),"k")
     #print("unit v.B= ",round(Bi/mB,3),"i ",round(Bj/mB,3),"j ",round(Bk/mB,3),"k")
-    print("A dot B= ",round(AdB,3))
+    print("A dot B= ",format(AdB,'.3f'))
     print("A x B= ",AcB[0],"i ",AcB[1],"j ",AcB[2],"k ")
     #print("A cross B= ",round(Aj*Bk-Ak*Bj,3),"i ",round(Ak*Bi-Ai*Bk,3),"j ",round(Ai*Bj-Aj*Bi,3),"k")
     return
@@ -82,11 +82,11 @@ def vectoramplifier():
     Ck=float(input("k= "))
     mC=float(math.sqrt(abs(Ci*Ci)+abs(Cj*Cj)+abs(Ck*Ck)))
     unvC=[Ci/mC,Cj/mC,Ck/mC]
-    displunvC=[round(x,3) for x in unvC]
+    displunvC=[format(x,'.3f') for x in unvC]
     print(displunvC)
     print("Input Scalar Coeff.")
     scalC=float(input("Scalar= "))
-    tunvC = [round((scalC*x),3) for x in unvC]
+    tunvC = [format((scalC*x),'.3f') for x in unvC]
     print(" ")
     print("Amplified Vector=")
     print(tunvC[0],"i ",tunvC[1],"j ",tunvC[2],"k ")
