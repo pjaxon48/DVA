86c1059c90b3cec2ea886b3a2dead5d74392d384
pjaxon48 <59519727+pjaxon48@users.noreply.github.com>
2023-09-28 13:25:51 -0400

    Update DVA.py

--- DVA.py
+++ DVA.py
@@ -1,7 +1,7 @@
-#    Dease Vector Accelerator, a simple micropython-calculator-compatible tool to make
-#    basic vector algebra applications in Engineering Statics easier.
+#    Dease Stress Accelerator, a simple micropython-calculator-compatible tool to make
+#    stress transformation problems in Machine Design easier.
 #
-#    Copyright (C) 2022  Philip Dease
+#    Copyright (C) 2023  Philip Dease
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
@@ -16,17 +16,22 @@
 #    You should have received a copy of the GNU General Public License
 #    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-import math
+global precision
+precision = int(3)
 
 def waiter():
     input("EXE to cont.")
     return
 
-def rounder(roundme):
-    # Yes, this sucks. But it should work on any MicroPython calculator.
-    roundme = roundme * 1000
-    roundme = float(int(roundme))
-    roundme = roundme / 1000
+def rounder(rounder):
+    global precision
+    # forced decimal (NO FLOAT)
+    factor = pow(10,precision)
+    rounder1 = int(rounder * factor)
+    rounder2 = int(rounder) * factor
+    rounder2 = rounder1 - rounder2
+    rounder1 = rounder1 // factor;
+    roundme = str(rounder1) + "." + str(abs(rounder2))
     return roundme
 
 def blanker():
@@ -38,62 +43,64 @@ def blanker():
     print(" ")
     return
 
+def precise():
+    blanker()
+    global precision
+    number = input("Decimal Precision: ")
+    if number != '':
+        number = int(number)
+        precision = number
+    return
+
 def selector():
-    print("Dease Vector")
-    print("Accelerator: v0.2c")
+    print("Dease Stress")
+    print("Accelerator: v0.1")
     print(" ")
-    print("[1] 2 Vectors") 
-    print("[2] Vec. Amplifier")
+    print("[1] Princ. Stress") 
+    print("[2] Von Mises Eq")
     choice=int(input("Choose (3 to quit)"))
     if  choice == 1:
-        twovectors()
+        princstress()
     if  choice == 2:
         vectoramplifier()
     if  choice == 3:
         quit()
     return 
 
-def twovectors():
+def princstress():
+    precise()
     blanker()
-    print("Input First Vector")
-    Ai=float(input("Ai= "))
-    Aj=float(input("Aj= "))
-    Ak=float(input("Ak= "))
-    print("Input Second Vector")
-    Bi=float(input("Bi= "))
-    Bj=float(input("Bj= "))
-    Bk=float(input("Bk= ")) 
-    mA=float(math.sqrt(abs(Ai*Ai)+abs(Aj*Aj)+abs(Ak*Ak)))
-    mB=float(math.sqrt(abs(Bi*Bi)+abs(Bj*Bj)+abs(Bk*Bk)))
-    AdB=float(Ai*Bi+Aj*Bj+Ak*Bk)
-    mAmB=float(mA*mB)
-    unvA=[rounder(Ai/mA),rounder(Aj/mA),rounder(Ak/mA)]
-    unvB=[rounder(Bi/mB),rounder(Bj/mB),rounder(Bk/mB)]
-    AcB=[rounder(Aj*Bk-Ak*Bj),rounder(Ak*Bi-Ai*Bk),rounder(Ai*Bj-Aj*Bi)]
-    print("magn.A= ",rounder(mA))
-    print("magn.B= ",rounder(mB))
-    print("unit v.A= ",unvA[0],"i ",unvA[1],"j ",unvA[2],"k ")
-    print("unit v.B= ",unvB[0],"i ",unvB[1],"j ",unvB[2],"k ")
-    print("A dot B= ",rounder(AdB))
-    print("A x B= ",AcB[0],"i ",AcB[1],"j ",AcB[2],"k ")
+    print("Input Stresses:")
+    oxx=float(input("@xx= "))
+    oyy=float(input("@yy= "))
+    txy=float(input("txy= "))
+    oavg = (oxx+oyy)/2
+    tmax = pow((pow((oxx-oyy)/2,2)+pow(txy,2)),0.5)
+    o1 = oavg + tmax
+    o2 = oavg - tmax
+    ov = pow((pow(o1,2)+pow(o2,2)-(o1*o2)),0.5)
+    print(" ")
+    print("@1= ",rounder(o1))
+    print("@2= ",rounder(o2))
+    print("@avg= ",rounder(oavg))
+    print("tmax= ",rounder(tmax))
+    print("@v= ",rounder(ov))
     return
 
 def vectoramplifier():
+    precise()
     blanker()
-    print("Input Components")
-    Ci=float(input("i= "))
-    Cj=float(input("j= "))
-    Ck=float(input("k= "))
-    mC=float(math.sqrt(abs(Ci*Ci)+abs(Cj*Cj)+abs(Ck*Ck)))
-    unvC=[Ci/mC,Cj/mC,Ck/mC]
-    displunvC=[rounder(x) for x in unvC]
-    print(displunvC)
-    print("Input Scalar Coeff.")
-    scalC=float(input("Scalar= "))
-    tunvC = [rounder((scalC*x)) for x in unvC]
+    o1=float(input("@1= "))
+    o2=float(input("@2= "))
+    o3=float(input("@3= "))
+    oy =float(input("@y= "))
+    ov = pow((pow(o1,2)+pow(o2,2)+pow(o3,2)-(o1*o2)-(o2*o3)-(o3*o1)),0.5)
+    fos = oy/ov
     print(" ")
-    print("Amplified Vector=")
-    print(tunvC[0],"i ",tunvC[1],"j ",tunvC[2],"k ")
+    print("Von Mises Equiv=")
+    print("@v/e=",rounder(ov))
+    print("F.O.S= ")
+    print("N= ",rounder(fos))
     return
 
 blanker()
